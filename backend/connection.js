d7ba53ede26f5ec71bb23c3d26d88abad6ca372d
Mohammad Asif <100487784+asif-0007@users.noreply.github.com>
2023-08-25 03:02:20 +0530

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db_url = require('../secret');
+//const db_url = require('../secret');
 const db_url = "mongodb+srv://admin:developers433@cluster0.cdgm0tn.mongodb.net/?retryWrites=true&w=majority"
 
 
